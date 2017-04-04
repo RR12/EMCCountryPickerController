23ccc9de04bbb9ac33ecd6264991983837ce2dff
iqbalansyori <iqbal@icehousecorp.com>
2017-04-04 16:51:35 +0700

    Add carthage

---
+++ EMCCountryPickerControllerDemo/Pods/Target Support Files/EMCCountryPickerController/EMCCountryPickerController-umbrella.h
@@ -0,0 +1,13 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "EMCCountry.h"
+#import "EMCCountryDelegate.h"
+#import "EMCCountryManager.h"
+#import "EMCCountryPickerController.h"
+#import "UIImage+UIImage_EMCImageResize.h"
+
+FOUNDATION_EXPORT double EMCCountryPickerControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char EMCCountryPickerControllerVersionString[];
+
